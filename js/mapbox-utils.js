c8278453d2156fe3ce41df14c4c647bee0a9a437
anmart <anmartdevelopers@gmail.com>
2022-12-11 00:56:20 +0300

    Geojson functionalities

---
+++ js/mapbox-utils.js
@@ -0,0 +1,149 @@
+const STYLES = {
+    red: {
+      url: "mapbox://styles/thijssondag/ck6eylpl67c971iqh6m0cbt6s",
+      penColor: "#8D241B",
+      markerColor: "#BC5148",
+      overlayTitleColor: "#FFFFFF",
+      overlaySubtitleColor: "#FFF8F8",
+      styleName: "ck6eylpl67c971iqh6m0cbt6s",
+      gradient: {
+        start: "rgba(218, 185, 185, 0)",
+        stop: "rgba(218, 185, 185, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(218, 185, 185, 0.8)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ] 
+    },
+    blue: {
+      url: "mapbox://styles/thijssondag/ck6f13i3z02fa1imqofgdxshs",
+      penColor: "#376B67",
+      markerColor: "#609B96",
+      overlayTitleColor: "#FFFFFF",
+      overlaySubtitleColor: "#FFF8F8",
+      styleName: "ck6f13i3z02fa1imqofgdxshs",
+      gradient: {
+        start: "rgba(169, 208, 205, 0)",
+        stop: "rgba(169, 208, 205, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(169, 208, 205, 0.8)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+    darkblue: {
+      url: "mapbox://styles/thijssondag/ck6e2njsx6ihl1iqh84r7pfa1",
+      penColor: "#ddbb65",
+      markerColor: "#ddbb65",
+      overlayTitleColor: "#ddbb65",
+      overlaySubtitleColor: "#ddbb65",
+      styleName: "ck6e2njsx6ihl1iqh84r7pfa1",
+      gradient: {
+        start: "rgba(54, 71, 99, 0)",
+        stop: "rgba(54, 71, 99, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(54, 71, 99, 0.2)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+    lightblue: {
+      url: "mapbox://styles/thijssondag/ck6dzdd5z0z2l1jmkuleu2cqp",
+      penColor: "#F26D5B",
+      markerColor: "#F26D5B",
+      overlayTitleColor: "#FFFFFF",
+      overlaySubtitleColor: "#FFF8F8",
+      styleName: "ck6dzdd5z0z2l1jmkuleu2cqp",
+      gradient: {
+        start: "rgba(160, 199, 238, 0)",
+        stop: "rgba(160, 199 ,238, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(150,194,237,0.8)'],
+        //[0.7966, 'rgba(190,217,244,0.51)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+    mintgreen: {
+      url: "mapbox://styles/thijssondag/ck6f0hxhs2jm21iq5kqokx7u8",
+      penColor: "#376B67",
+      markerColor: "#609B96",
+      overlayTitleColor: "#125913",
+      overlaySubtitleColor: "#125913",
+      styleName: "ck6f0hxhs2jm21iq5kqokx7u8",
+      gradient: {
+        start: "rgba(202, 237, 216, 0)",
+        stop: "rgba(202, 237, 216, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(202, 237, 216, 0.8)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+    green: {
+      url: "mapbox://styles/thijssondag/ck6f1wkxy1yx31imtlwp7w08o",
+      penColor: "#376B67",
+      markerColor: "#609B96",
+      overlayTitleColor: "#FFFFFF",
+      overlaySubtitleColor: "#FFFFFF",
+      styleName: "ck6f1wkxy1yx31imtlwp7w08o",
+      gradient: {
+        start: "rgba(170, 208, 191, 0)",
+        stop: "rgba(170, 208, 191, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(170, 208, 191, 0.8)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+    grey: {
+      url: "mapbox://styles/thijssondag/ck6f2xosk1vll1imzg042pv4x",
+      penColor: "#F26D5B",
+      markerColor: "#F26D5B",
+      overlayTitleColor: "#3C3C3C",
+      overlaySubtitleColor: "#3C3C3C",
+      styleName: "ck6f2xosk1vll1imzg042pv4x",
+      gradient: {
+        start: "rgba(239, 240, 240, 0)",
+        stop: "rgba(239, 240, 240, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(239, 240, 240, 0.8)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+    natural: {
+      url: "mapbox://styles/thijssondag/ck6f2hac10p0y1ir5dr4w9wqu",
+      penColor: "#2B554F",
+      markerColor: "#2B554F",
+      overlayTitleColor: "#2B554F",
+      overlaySubtitleColor: "#2B554F",
+      styleName: "ck6f2hac10p0y1ir5dr4w9wqu",
+      gradient: {
+        start: "rgba(241, 236, 229, 0)",
+        stop: "rgba(241, 236, 229, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(241, 236, 229, 0.8)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+    purple: {
+      url: "mapbox://styles/thijssondag/ck6f22h0v1usa1imzskjvcnqo",
+      penColor: "#2E294E",
+      markerColor: "#2E294E",
+      overlayTitleColor: "#2E294E",
+      overlaySubtitleColor: "#2E294E",
+      styleName: "ck6f22h0v1usa1imzskjvcnqo",
+      gradient: {
+        start: "rgba(234, 224, 250, 0)",
+        stop: "rgba(234, 224, 250, 1)",
+      },
+      statGradient: [
+        [0.0, 'rgba(234, 224, 250, 0.8)'],
+        [1.0, 'rgba(255,255,255,0)'],
+      ]
+    },
+  };
+
+  
